d736808f6ad3e8e6a49bb8e320536c2d926c05ac
Jake Lowen <jakelowen@gmail.com>
2018-09-13 14:25:19 -0500

    Resolves #61.

--- src/client/src/views/u/OrgSidebarInfo.jsx
+++ src/client/src/views/u/OrgSidebarInfo.jsx
@@ -1,36 +1,46 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 // import PropTypes from 'prop-types';
-import { Query } from 'react-apollo';
-import ORG_DETAILS from '../../data/queries/orgInfo';
-import { withRouter } from 'react-router-dom';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { faPhone, faAt } from '@fortawesome/fontawesome-pro-light';
+import { Query } from "react-apollo";
+import ORG_DETAILS from "../../data/queries/orgInfo";
+import { withRouter } from "react-router-dom";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import { faPhone, faAt } from "@fortawesome/fontawesome-pro-light";
 // import { isLoggedIn, hasOrgAccess } from '../utils/auth';
-import ReactRouterPropTypes from 'react-router-prop-types';
+import ReactRouterPropTypes from "react-router-prop-types";
 
 class OrgSidebarInfo extends PureComponent {
   render() {
     return (
-      <Query query={ORG_DETAILS} variables={{ where: { id: this.props.match.params.orgSlug } }}>
+      <Query
+        query={ORG_DETAILS}
+        variables={{ where: { id: this.props.match.params.orgSlug } }}
+      >
         {({ loading, error, data: { organization } }) => {
           if (loading) return <div className="loader" />;
           if (error) return <p>Error!</p>;
           return (
             <div>
               <div className="content box">
-                <h5 className="title">You are an ambassador for {organization.name}</h5>
+                <h5 className="title">
+                  You are an ambassador for {organization.name}
+                </h5>
                 <p>
                   <strong>For assistance contact:</strong>
                   <br />
                   {organization.contact_name} <br />
                   {organization.contact_phone ? (
-                    <div>
-                      <FontAwesomeIcon icon={faPhone} />{' '}
-                      <span style={{ paddingLeft: 20 }}>{organization.contact_phone}</span> <br />
-                    </div>
+                    <span>
+                      <FontAwesomeIcon icon={faPhone} />{" "}
+                      <span style={{ paddingLeft: 20 }}>
+                        {organization.contact_phone}
+                      </span>{" "}
+                      <br />
+                    </span>
                   ) : null}
                   <FontAwesomeIcon icon={faAt} />
-                  <span style={{ paddingLeft: 20 }}>{organization.contact_email}</span>
+                  <span style={{ paddingLeft: 20 }}>
+                    {organization.contact_email}
+                  </span>
                 </p>
               </div>
             </div>
@@ -42,7 +52,7 @@ class OrgSidebarInfo extends PureComponent {
 }
 
 OrgSidebarInfo.propTypes = {
-  match: ReactRouterPropTypes.match.isRequired,
+  match: ReactRouterPropTypes.match.isRequired
 };
 
 export default withRouter(OrgSidebarInfo);
--- src/client/src/views/u/TaskModal.jsx
+++ src/client/src/views/u/TaskModal.jsx
@@ -1,39 +1,48 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import SchemaForm from './SchemaForm';
-import { Mutation } from 'react-apollo';
-import UPDATE_TASK from '../../data/mutations/updateTask';
-import POINTS_PROFILE_USER_ORG_LIMITED from '../../data/queries/pointsProfileUserOrgLimited';
-import { getUserEmail } from '../../utils/auth';
-import { withRouter } from 'react-router-dom';
-import ReactRouterPropTypes from 'react-router-prop-types';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { faThumbsUp } from '@fortawesome/fontawesome-pro-solid';
-import { faSquare, faCheckSquare } from '@fortawesome/fontawesome-pro-light';
-import { sortBy } from 'lodash';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import SchemaForm from "./SchemaForm";
+import { Mutation } from "react-apollo";
+import UPDATE_TASK from "../../data/mutations/updateTask";
+import POINTS_PROFILE_USER_ORG_LIMITED from "../../data/queries/pointsProfileUserOrgLimited";
+import { getUserEmail } from "../../utils/auth";
+import { withRouter } from "react-router-dom";
+import ReactRouterPropTypes from "react-router-prop-types";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import { faThumbsUp } from "@fortawesome/fontawesome-pro-solid";
+import { faSquare, faCheckSquare } from "@fortawesome/fontawesome-pro-light";
+import { sortBy } from "lodash";
 
 class TaskModal extends PureComponent {
   state = {
-    selectedTask: null,
+    selectedTask: null
   };
 
   render() {
     const { potentialVoter } = this.props;
-    const sortedTasks = sortBy(potentialVoter.tasks, 'status').reverse();
-    const sequenceSortedTasks = sortBy(sortedTasks, 'sequence');
+    const sortedTasks = sortBy(potentialVoter.tasks, "status").reverse();
+    const sequenceSortedTasks = sortBy(sortedTasks, "sequence");
     return (
-      <div className={classNames('modal', { 'is-active': this.props.open })}>
+      <div
+        className={classNames("modal", {
+          "is-active": this.props.open
+        })}
+      >
         <div className="modal-background" onClick={() => this.props.close()} />
 
         <div className="modal-card">
           <header className="modal-card-head">
             <p className="modal-card-title">Tasks</p>
-            <button className="delete" aria-label="close" onClick={() => this.props.close()} />
+            <button
+              className="delete"
+              aria-label="close"
+              onClick={() => this.props.close()}
+            />
           </header>
           <section className="modal-card-body">
             <div className="content">
-              {potentialVoter.voterFileRecord && potentialVoter.voterFileRecord.state_file_id ? (
+              {potentialVoter.voterFileRecord &&
+              potentialVoter.voterFileRecord.state_file_id ? (
                 <Mutation
                   mutation={UPDATE_TASK}
                   refetchQueries={[
@@ -41,36 +50,61 @@ class TaskModal extends PureComponent {
                       query: POINTS_PROFILE_USER_ORG_LIMITED,
                       variables: {
                         email: getUserEmail(),
-                        org_id: this.props.match.params.orgSlug,
-                      },
-                    },
+                        org_id: this.props.match.params.orgSlug
+                      }
+                    }
                   ]}
                 >
                   {updateTask => (
                     <div>
                       {this.state.selectedTask !== null ? (
-                        <SchemaForm
-                          key={this.state.selectedTask.id}
-                          taskId={this.state.selectedTask.id}
-                          schema={this.state.selectedTask.form_schema}
-                          point_value={this.state.selectedTask.point_value}
-                          submitFn={values =>
-                            updateTask({
-                              variables: {
-                                id: this.state.selectedTask.id,
-                                status: 'COMPLETE',
-                                form_data: values,
-                              },
-                            }).then(() => this.props.close())
-                          }
-                        />
+                        <div>
+                          {this.state.selectedTask.status === "COMPLETE" ? (
+                            <div>
+                              <p>
+                                This task is already completed. If you wish to
+                                reset it, please click below
+                              </p>
+                              <button
+                                className="button is-danger"
+                                onClick={() =>
+                                  updateTask({
+                                    variables: {
+                                      id: this.state.selectedTask.id,
+                                      status: "INCOMPLETE",
+                                      form_data: {}
+                                    }
+                                  }).then(() => this.props.close())
+                                }
+                              >
+                                Reset Task
+                              </button>
+                            </div>
+                          ) : (
+                            <SchemaForm
+                              key={this.state.selectedTask.id}
+                              taskId={this.state.selectedTask.id}
+                              schema={this.state.selectedTask.form_schema}
+                              point_value={this.state.selectedTask.point_value}
+                              submitFn={values =>
+                                updateTask({
+                                  variables: {
+                                    id: this.state.selectedTask.id,
+                                    status: "COMPLETE",
+                                    form_data: values
+                                  }
+                                }).then(() => this.props.close())
+                              }
+                            />
+                          )}
+                        </div>
                       ) : (
                         <div>
                           {sequenceSortedTasks.length > 0 ? (
                             <div className="content">
                               <h4>
-                                Click a task below to preview or complete. Completed tasks earn
-                                points!
+                                Click a task below to preview or complete.
+                                Completed tasks earn points!
                               </h4>
                               <table className="table">
                                 <thead>
@@ -83,28 +117,36 @@ class TaskModal extends PureComponent {
                                 <tbody>
                                   {sequenceSortedTasks.map(task => (
                                     <tr
-                                      className={classNames('task_row', {
-                                        complete: task.status === 'COMPLETE',
+                                      className={classNames("task_row", {
+                                        complete: task.status === "COMPLETE"
                                       })}
                                       key={task.id}
                                       onClick={() => {
-                                        if (task.status !== 'COMPLETE') {
-                                          this.setState({ selectedTask: task });
-                                        }
+                                        // if (task.status !== 'COMPLETE') {
+                                        this.setState({
+                                          selectedTask: task
+                                        });
+                                        // }
                                       }}
                                     >
                                       <td
-                                        className={classNames('task_title', {
-                                          complete: task.status === 'COMPLETE',
+                                        className={classNames("task_title", {
+                                          complete: task.status === "COMPLETE"
                                         })}
                                       >
                                         {task.description}
                                       </td>
                                       <td>
-                                        {task.status === 'COMPLETE' ? (
-                                          <FontAwesomeIcon icon={faCheckSquare} size="2x" />
+                                        {task.status === "COMPLETE" ? (
+                                          <FontAwesomeIcon
+                                            icon={faCheckSquare}
+                                            size="2x"
+                                          />
                                         ) : (
-                                          <FontAwesomeIcon icon={faSquare} size="2x" />
+                                          <FontAwesomeIcon
+                                            icon={faSquare}
+                                            size="2x"
+                                          />
                                         )}
                                       </td>
                                       <td>{task.point_value}</td>
@@ -119,10 +161,13 @@ class TaskModal extends PureComponent {
                                 <FontAwesomeIcon icon={faThumbsUp} size="10x" />
                               </div>
                               <h2>
-                                You&apos;ve completed all the currently assigned tasks for this
-                                voter!
+                                You&apos;ve completed all the currently assigned
+                                tasks for this voter!
                               </h2>
-                              <p>Check back again soon as we are always adding more tasks</p>
+                              <p>
+                                Check back again soon as we are always adding
+                                more tasks
+                              </p>
                             </div>
                           )}
                         </div>
@@ -135,12 +180,13 @@ class TaskModal extends PureComponent {
                   <h4>Match this person to the voter file. </h4>
                   <p>
                     <strong>
-                      Your first task is to make sure this person is registered to vote!
+                      Your first task is to make sure this person is registered
+                      to vote!
                     </strong>
                   </p>
                   <p>
-                    First, check to see if you can match them up with a record in the public voter
-                    file provided by your county.
+                    First, check to see if you can match them up with a record
+                    in the public voter file provided by your county.
                   </p>
                   <button
                     className="button is-link submit-button is-fullwidth"
@@ -153,10 +199,12 @@ class TaskModal extends PureComponent {
                     Search for matching voter records
                   </button>
                   <p style={{ paddingTop: 10 }}>
-                    If they aren’t there, it might be that they aren’t registered to vote. You
-                    should contact them and ask them if they’ve registered recently, or if you might
-                    have their name or city wrong. If they aren’t registered, or they don’t know,
-                    encourage them to go to ksvotes.org to check their registration or to register
+                    If they aren’t there, it might be that they aren’t
+                    registered to vote. You should contact them and ask them if
+                    they’ve registered recently, or if you might have their name
+                    or city wrong. If they aren’t registered, or they don’t
+                    know, encourage them to go to ksvotes.org to check their
+                    registration or to register
                   </p>
                 </div>
               )}
@@ -178,7 +226,7 @@ TaskModal.propTypes = {
   close: PropTypes.func.isRequired,
   potentialVoter: PropTypes.object.isRequired,
   match: ReactRouterPropTypes.match.isRequired,
-  openVoterSearchModal: PropTypes.func.isRequired,
+  openVoterSearchModal: PropTypes.func.isRequired
 };
 
 export default withRouter(TaskModal);
